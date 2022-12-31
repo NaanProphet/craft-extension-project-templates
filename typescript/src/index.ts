3c0af9c97a41ce939e2daa07d0903dc2a024d9a1
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2022-12-31 14:23:13 -0500

    IAST to ITRANS prototype
    doesn't work as expected because of https://github.com/craftdocs/craft-extension-inspirations/issues/6 and https://github.com/craftdocs/craft-extension-api/issues/2

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,5 +1,6 @@
 import "./style.css"
 import craftXIconSrc from "./craftx-icon.png"
+import {textContent} from "./utils";
 
 craft.env.setListener((env) => {
   switch (env.colorScheme) {
@@ -15,12 +16,42 @@ craft.env.setListener((env) => {
 window.addEventListener("load", () => {
   const button = document.getElementById('btn-execute');
 
-  button?.addEventListener("click", () => {
+  button?.addEventListener("click", async () => {
+
+    const response = await craft.editorApi.getSelection();
+    const dataBlock = response.data!;
+    const currentPage = await craft.dataApi.getCurrentPage();
+    const dataText = textContent(dataBlock[0]);
+
+    const theData = {
+      "source": "IAST",
+      "target": "Itrans",
+      "text": dataText,
+      "nativize": true,
+      "postOptions": [],
+      "preOptions": []
+    };
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(theData)
+    };
+    const response2 = await fetch('http://localhost:8085/api/convert', settings);
+    const response2Data = await response2.text();
+    const response3 = response2Data.replace(/<br\/>/g,"\n");
+
+    // craft.editorApi.openURL('http://localhost:8085');
+
     const block = craft.blockFactory.textBlock({
-      content: "Hello world!"
+      content: response3
     });
 
-    craft.dataApi.addBlocks([block]);
+    const currentPageId = currentPage.data?.id!;
+    const location = craft.location.afterBlockLocation(currentPageId, dataBlock[0].id);
+    craft.dataApi.addBlocks([block], location);
   })
   const logoImg = document.getElementById('craftx-logo') as HTMLImageElement
   logoImg.src = craftXIconSrc;
